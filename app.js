f25dd2686bd7904d23945259b35141d7bd46ad7f
quiquequique <eabramzon@gmail.com>
2020-03-17 16:57:07 -0300

    estos 3 archivos son mejorables pero ya es funcional

---
+++ app.js
@@ -0,0 +1,76 @@
+let tareas = require('./tareas');
+let accion = process.argv[2];
+let chalk = require('chalk');
+//console.log(tareas.leer()[2])
+//console.log(process.argv[2])
+
+switch(accion){
+    case 'listar':
+        console.log('tareas por ejecutar:')
+        let mistareas = tareas.leer()
+        //console.log(mistareas.length);
+        mistareas.forEach(function(tarea){
+            console.log (chalk.blue.underline(tarea.titulo) +": "+ chalk.cyan(tarea.estado));
+        });
+
+    break
+    // reemplazar ciclo for por forEach: done!
+
+    case undefined : console.log(chalk.red.bgWhite('Debes ingresar un comando: listar, crear, etc!!'));
+    break
+
+    case 'crear':
+    let tareaNueva = {
+        titulo: process.argv[3],
+        estado: 'pendiente'
+    }
+    tareas.guardarTarea(tareaNueva)
+    //console.log('creaste una nueva tarea:  ' + tareaNueva.titulo +':   ' + tareaNueva.estado);
+    break
+    // corregir crear para que no haga duplicados!!
+    //console.log(process.argv[3]);
+
+
+
+    case 'filtrar':
+
+            let estadoActual = process.argv[3]
+            let filtradas =  tareas.filtrarPorEstado(estadoActual);
+
+            console.log(chalk.red.bold.bgWhite('Estas son tus tareas pendientes:'));
+
+            filtradas.forEach(function(tarea){
+                console.log(chalk.blue.underline(tarea.titulo) + chalk.blue(': ') + (chalk.cyan(tarea.estado)));
+
+            })
+            //console.log(filtradas) = escribe todo el array de una.
+        break
+
+        case 'cambEstado':
+            let tareaCambiada = {
+                titulo: process.argv[3],
+                estado: process.argv[4]
+            }
+            tareas.cambiarTarea(tareaCambiada)
+            break
+
+            case 'borrar':
+            let tareaBorrada = process.argv[3];
+            tareas.borrarTarea(tareaBorrada);
+            
+            break
+
+            default: console.log(chalk.red.bgWhite('Argumento no valido!!'));
+
+        }
+        
+        
+//modificacion pata test git
+//falta borrar, borrar terminados, Help y shortcuts
+//chalk instalar
+
+//inicializar proyecto de Node
+
+// npm install
+
+// tareas terminadas
---
+++ tareas.js
@@ -0,0 +1,76 @@
+let chalk = require('chalk');
+const fs = require('fs');
+let tareas = {
+    archivo: 'tareas.json',
+    leer: function() {
+        let string = fs.readFileSync(this.archivo, 'utf-8');
+
+        return JSON.parse(string)
+    },
+    // crear funcion que escribe array en el JSON
+    escribirJson: function(tareas) {
+
+        let tareasJson = JSON.stringify(tareas, null, ' ')
+        fs.writeFileSync(this.archivo, tareasJson)
+
+
+    },
+    // GUARDAR TAREA
+    // 1.traer todas las tareas
+    // 2. verifica si la tarea ya exista
+    //guardar el nuevo array de tareas pisando el viejo
+
+    guardarTarea: function (tareaNueva){
+        let todasLasTareas = this.leer()
+        let yaExiste = todasLasTareas.some(tarea => {
+            return JSON.stringify(tareaNueva) == JSON.stringify(tarea);
+            //[2, 5, 8, 1, 4].some(elem => elem > 10);  // false ademas no se pueden comparar objetos sin pasarlos  a string ya lo entendere
+            //[12, 5, 8, 1, 4].some(elem => elem > 10); // true
+        });
+        if(yaExiste){
+            console.log(chalk.red.bold.underline('ERROR  !!') + '  ' + chalk.blue('Esta tarea ya fue creada'));
+        }else{todasLasTareas.push(tareaNueva)
+            this.escribirJson(todasLasTareas)
+            console.log(chalk.blue.underline('creaste una nueva tarea:') +'  ' + chalk.cyan('Nueva tarea:  ' + tareaNueva.titulo +':   ' + 'Estado:  ' + tareaNueva.estado));
+            };
+
+    },
+
+    cambiarTarea: function (tareaCambiada){     //cambia el estado de la tarea 
+        let todasLasTareas = this.leer();
+        let indexOfTareaCambiada = todasLasTareas.findIndex(i => i.titulo == tareaCambiada.titulo);
+        todasLasTareas.splice(indexOfTareaCambiada, 1, tareaCambiada);
+        this.escribirJson(todasLasTareas);
+
+        },
+            //Ej. de metodo splice!!
+            //languages.splice(1, 1, 'Python');
+            //[{hello: 'world',foo: 'bar'};{hello: 'stevie',foo: 'baz']
+            //var indexOfStevie = myArray.findIndex(i => i.hello === "stevie");
+
+
+    borrarTarea: function (tareaBorrada){     //borra tarea
+        let todasLasTareas = this.leer();
+        let indexOfTareaBorrada = todasLasTareas.findIndex(i => i.titulo == tareaBorrada);
+        if (indexOfTareaBorrada > -1){
+            todasLasTareas.splice(indexOfTareaBorrada, 1);
+            this.escribirJson(todasLasTareas);
+            console.log(chalk.blue.underline('La tarea: ' + tareaBorrada )+ chalk.cyan('  a sido eliminada!!'));
+        }else{
+            console.log(chalk.red.bold.underline('La tarea no existe!!!'));
+        }
+
+
+
+                },
+
+    filtrarPorEstado: function (estadoActual){
+        let todasLasTareas = this.leer()
+        return todasLasTareas.filter(function(cadaTarea){
+           return cadaTarea.estado == estadoActual
+        })
+    }
+}
+// agregar borrar, cambiar estado, help
+
+module.exports = tareas;
